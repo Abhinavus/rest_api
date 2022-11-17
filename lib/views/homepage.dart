974bef8cf3fa097f09d52d2646b2ccf98616b2b9
Abhinavus <sabhi.sncs404@gmail.com>
2022-11-17 12:30:04 +0530

    first commit

---
+++ lib/views/homepage.dart
@@ -0,0 +1,70 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:rest_api/services/remote_service.dart';
+
+import '../models/post.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  List<Post>? posts;
+var isLoaded =false;
+
+
+@override
+  void initState() {
+    super.initState();
+    getData();
+  }
+  getData() async {
+    posts = await RemoteService().getPosts();
+    if(posts!= null){
+      setState(() {
+        isLoaded =true;
+      });
+    }
+
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(title: Text('Post'),),
+  body: Visibility(
+    visible: isLoaded,
+    // ignore: sort_child_properties_last
+    child: ListView.builder(
+      itemCount: posts?.length,
+      itemBuilder: (context, index) {
+      return 
+          Row(
+            children: [
+              Container( width: 50, height: 50,
+                child:
+                    Image.network(posts![index].url),
+                    
+                    
+                    
+                  
+
+              ),
+            ],
+          );
+      
+      
+
+
+    },),
+    replacement: const Center(child: CircularProgressIndicator(),),
+  ),
+    );
+  }
+  
+  
+}
